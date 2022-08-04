add64f4c412d19c4e28b3863ba13e3331c7f33a5
CarlosGp <carlos200012-@hotmail.com>
2022-08-04 12:04:05 -0500

    Posi

--- retiro.php
+++ retiro.php
@@ -1,10 +1,50 @@
 <?php
     include 'includes/plantillas/header.php';
 ?>
-<h1>Retiro</h1>
-<div class="botones">
-    <div class="box"></div>
-</div>
+    <h1>Retiro</h1>
+
+    <div class="contenedor grid-3">
+        <div class="valores-left">
+            <div class="box">
+                <a href="#" class="valor">$5</a>
+            </div>
+        </div>
+        <div class="valores-right">
+            <div class="box2">
+                <a href="#" class="valor">$100</a>
+            </div>
+        </div>
+
+        <div class="valores-left">
+            <div class="box">
+                <a href="#" class="valor">$20</a>
+            </div>
+        </div>
+        <div class="valores-right">
+            <div class="box2">
+                <a href="#" class="valor">$200</a>
+            </div>
+        </div>
+
+        <div class="valores-left">
+            <div class="box">
+                <a href="#" class="valor">$50</a>
+            </div>
+        </div>
+        <div class="valores-right">
+            <div class="box2">
+                <a href="#" class="valor">$300</a>
+            </div>
+        </div>
+
+        <div class="pantalla-central">
+            <label class="texto" for="">Valor ingresado</label>
+            <h3 class="pantalla">$220</h3>
+        </div>
+        <div>
+
+        </div>
+    </div>
 <?php
     include 'includes/plantillas/footer.php';
 ?>
