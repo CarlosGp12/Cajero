57250fb67243fb6d84b2861ebfd8de2ad0a84a5f
CarlosGp <carlos200012-@hotmail.com>
2022-08-04 10:38:54 -0500

    Primer cajero

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    include 'includes/plantillas/header.php';
+?>
+
+    <main>
+        <h1 class="h1">Elija la operacion</h1>
+        <div class="contenedor grid">
+            
+            <a href="retiro.php" class="contenido enlace"> 
+                <img src="img/salida.svg" class="iconos" alt="">
+                Retirar dinero
+            </a>
+        
+            
+            <a href="#" class="contenido enlace">
+                <img src="img/transferencia.svg" class="iconos" alt="">
+                Transferencia
+            </a>
+        
+            
+            <a href="#" class="contenido enlace">
+                <img src="img/entrada.svg" class="iconos" alt="">
+                Ingreso dinero
+            </a>
+        
+
+            <a href="#" class="contenido enlace">
+                <img src="img/lista.svg" class="iconos" alt="">
+                Consulta de datos
+            </a>
+
+        </div>
+    </main>
+<?php
+    include 'includes/plantillas/header.php';
+?>
---
+++ retiro.php
@@ -0,0 +1,10 @@
+<?php
+    include 'includes/plantillas/header.php';
+?>
+<h1>Retiro</h1>
+<div class="botones">
+    <div class="box"></div>
+</div>
+<?php
+    include 'includes/plantillas/footer.php';
+?>
