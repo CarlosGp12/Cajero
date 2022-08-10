5ae16e3dabec6014b6fbd2bbfaf9777664858e55
CarlosGp <carlos200012-@hotmail.com>
2022-08-10 13:52:18 -0500

    final

--- Cajero.sql
+++ Cajero.sql
@@ -32,12 +32,33 @@ create table saldo(
     references cuentas (id)
 );
 
+create table movimiento(
+	id int(11) NOT NULL auto_increment,
+    num_origen char(10),
+    num_destino char(10),
+    cantidad decimal(6,2),
+    fecha date,
+    primary key(id)
+);
+
+create table retiros(
+	id int(11) NOT NULL auto_increment,
+    cuentaId int(11),
+    cantidad decimal(6,2),
+    fecha date,
+    primary key(id)
+);
+
 -- drop table cuenta_ahorro;
 -- drop table cuenta_corriente;
+-- drop table movimiento;
+-- drop table retiros;
 
 select * from usuarios;
 select * from cuentas;
 select * from saldo;
+select * from movimiento;
+select * from retiros;
 
 insert into usuarios (usuario, nombre, cedula, num_tarjeta, clave) values ("CarlosGp", "Carlos","0923377972","0947854124", "1242");
 insert into usuarios (usuario, nombre, cedula, num_tarjeta, clave) values ("CesarMp", "Cesar","0923374788","0978623247", "4754");
@@ -47,7 +68,7 @@ insert into saldo (cuentaId, usuarioId, saldo) values (1 , 1, 1200);
 insert into saldo (cuentaId, usuarioId, saldo) values (2 , 1, 1000);
 insert into saldo (cuentaId, usuarioId, saldo) values (1 , 2, 900);
 insert into saldo (cuentaId, usuarioId, saldo) values (2 , 2, 700);
-insert into cuenta_corriente (usuarioId, saldo) values (2, 900);
+
 SELECT * FROM saldo WHERE (cuentaId = 1 AND usuarioId =1);
 
 UPDATE saldo SET saldo = saldo+100 where id=1;
